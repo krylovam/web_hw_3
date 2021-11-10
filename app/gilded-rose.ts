d1d2458093698f564695d6d4d429e145bb7928e4
Krylova Maria <47410435+krylovam@users.noreply.github.com>
2021-11-10 10:50:26 +0300

    fixed tabs

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -13,31 +13,31 @@ export class GildedRose {
     }
 
     updateQuality() : Array<Item> {
-	    for (let item of this.items) {
+        for (let item of this.items) {
             switch (item.name) {
                 case ItemNameEnum.agedBrie:
                     item.sellIn -= 1;
-					if (item.sellIn < MIN_VALUE & item.quality < MAX_QUALITY-1) {
-						item.quality += 2;
-					}
-					else if (item.quality < MAX_QUALITY) {
-						item.quality += 1;
-					}
+                    if (item.sellIn < MIN_VALUE & item.quality < MAX_QUALITY-1) {
+                        item.quality += 2;
+                    }
+                    else if (item.quality < MAX_QUALITY) {
+                        item.quality += 1;
+                    }
                     break
                 case ItemNameEnum.sulfuras:
                     break
                 case ItemNameEnum.backstagePasses:
-					item.sellIn -= 1;
-					if (item.sellIn < MIN_VALUE) {
-						item.quality = MIN_VALUE;
-						break;
-					}
-					if (item.quality  >= MAX_QUALITY) {
-						break;
-					}
-					if (item.sellIn < 5) {
+                    item.sellIn -= 1;
+                    if (item.sellIn < MIN_VALUE) {
+                        item.quality = MIN_VALUE;
+                        break;
+                    }
+                    if (item.quality  >= MAX_QUALITY) {
+                        break;
+                    }
+                    if (item.sellIn < 5) {
                         item.quality = Math.min(item.quality + 3, MAX_QUALITY);
-						break;
+                        break;
                     }
                     if (item.sellIn < 10) {
                         item.quality = Math.min(item.quality + 2, MAX_QUALITY);
@@ -47,22 +47,22 @@ export class GildedRose {
 					break;                    
                 case ItemNameEnum.conjured:
                     item.sellIn -= 1;
-					if (item.sellIn < MIN_VALUE && item.quality > MIN_VALUE + 1) {
-						item.quality -= 2;
-					}
-					else if (item.quality > MIN_VALUE) {
-						item.quality -= 1;
-					}
+                    if (item.sellIn < MIN_VALUE && item.quality > MIN_VALUE + 1) {
+                        item.quality -= 2;
+                    }
+                    else if (item.quality > MIN_VALUE) {
+                        item.quality -= 1;
+                    }
                     break
 				default:
-					item.sellIn -= 1;
-					if (item.sellIn < MIN_VALUE && item.quality > MIN_VALUE + 1) {
-						item.quality -= 2;
-					}
-					else if (item.quality > MIN_VALUE) {
-						item.quality -= 1;
-					}
-					break
+                    item.sellIn -= 1;
+                    if (item.sellIn < MIN_VALUE && item.quality > MIN_VALUE + 1) {
+                        item.quality -= 2;
+                    }
+                    else if (item.quality > MIN_VALUE) {
+                        item.quality -= 1;
+                    }
+                    break
             }
         }
 
